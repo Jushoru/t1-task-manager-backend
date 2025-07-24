737975ffa9225403834b824f0167cf29012bd654
Jushoru <smirnov.kirill.kaduy@gmail.com>
2025-07-24 16:57:33 +0300

    feat: add CORS connection

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import taskRoute from "./task.route";
+import cors from 'cors'
 
 const app = express();
 const PORT = 3001;
+
+app.use(cors());
 
 app.use(express.json());
 
